0990441c1bfc9b4b61c4ca9d08bf1b225bf9df43
zutanov <big_smoke9@mail.ru.com>
2021-11-16 02:28:39 +0600

    js intro

---
+++ script/script.js
@@ -0,0 +1,28 @@
+
+console.log("Привет! Меня зовут Замир.")
+console.log("Мне 29 лет.")
+console.log("Являюсь студентом курса " + '"FullStack"' + " из солнечного г. Ош.")
+console.log("Работаю территориальным менеджером продаж в международной табачной компании")
+
+const pre = "и"
+const name = "кот"
+console.log(pre + " днем " + pre + " ночью " + name + " ученый")
+
+    const word = "Программ"
+    const pc = "компьютер"
+    console.log("Чтоб считал " + pc + " быстро,")
+    console.log("Правила чтоб не забыл,")
+    console.log("Потрудился "+ word +"ист тут,")
+    console.log("Он "+ word +"у сочинил.")
+    console.log("Перед ним клавиатура,")
+    console.log("Мягко светится экран.")
+    console.log("На экране же — фигуры,")
+    console.log("Текст, таблица, схема, план.")
+    console.log(word + "ист " + word +"ы пишет,")
+    console.log("Чтоб " + pc + " мог затем")
+    console.log("Поиграть с любым мальчишкой,")
+    console.log("Тысячи решить проблем.")
+    console.log("И "+ word + "у за " + word + "ой")
+    console.log(word + "исты создают,")
+    console.log("Чтобы вашим папам, мамам")
+    console.log("Облегчил " + pc + " труд!")
